e6a0e3f1fd5e7bb7ca6c4bd12738de113ad67d96
k2lxn <jane.e.reynolds@gmail.com>
2015-08-10 18:54:54 -0700

    fixed nav disappearing on link click

--- mobile-nav.js
+++ mobile-nav.js
@@ -9,9 +9,12 @@ $(document).ready(function(){
 	});
 	
 	/* Pull menu up after user chooses (taps) a link */
-	$('nav ul li a').on('click', function(e) {
-		/*e.preventDefault();*/
-		menu.delay(300).slideToggle("fast");
-	});
+	if ($(window).width() < 415) {
+	
+		$('nav ul li a').on('click', function(e) {
+			/*e.preventDefault();*/
+			menu.delay(300).slideToggle("fast");
+		});
+	};
 
 });
